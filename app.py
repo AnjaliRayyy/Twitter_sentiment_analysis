039a31f3684a47a59b2e7a6782c8ccd2b94e9391
Anjali Ray <rayyyanjali@gmail.com>
2025-12-27 13:27:07 +0530

    Streamlit default css overriden

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ header {visibility: hidden;}
 footer {visibility: hidden;}
 </style>
 """
-
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.write(
     "Analyze the sentiment of a tweet using a trained Machine Learning model."
 )
